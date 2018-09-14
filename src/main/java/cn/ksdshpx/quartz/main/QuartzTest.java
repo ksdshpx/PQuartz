3c6315afd0d61c682c767cb7607bc978fd50c842
ksdshpx <southeast_px@163.com>
2018-09-14 13:55:24 +0800

    Quartz入门案例

---
+++ src/main/java/cn/ksdshpx/quartz/main/QuartzTest.java
@@ -0,0 +1,30 @@
+package cn.ksdshpx.quartz.main;
+
+import cn.ksdshpx.quartz.job.MyJob;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/9/14
+ * Time: 11:08
+ * Description:测试程序
+ */
+public class QuartzTest {
+    public static void main(String[] args) throws Exception {
+        //调度器
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        //任务
+        JobDetail jobDetail = JobBuilder.newJob(MyJob.class)
+                .withIdentity("job1", "group1")
+                .build();
+        //触发器
+        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1")
+                .startNow()
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(5).repeatForever())
+                .build();
+        scheduler.scheduleJob(jobDetail, trigger);
+        scheduler.start();
+    }
+}
